1b47541e3e559ea7be25ee5e248b8a769dea2f9b
Mark Pascua <qmevpascua@tip.edu.ph>
2022-11-03 17:05:05 +0800

    Adding colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static final Color mainbgcolor = const Color(0xFFE8F3FF);
+  static final Color shapecolor = const Color(0xFF508DF4);
+  static final Color white = const Color(0xFFFFFFFF);
+  static final Color textcolorlogin = const Color(0xFF888888);
+
+}
