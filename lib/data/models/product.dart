497430553ab9bf66d2a6d37876a40ae12b592b6c
Mark Pascua <qmevpascua@tip.edu.ph>
2022-11-04 23:12:22 +0800

    samples

--- lib/data/models/product.dart
+++ lib/data/models/product.dart
@@ -117,124 +117,124 @@ List<Product> demoProducts = [
     isFavourite: true,
     isPopular: true,
   ),
-  Product(
-    id: 6,
-    image: "assets/images/p1.png",
-    images: [
-      "assets/images/p1_2.jpg",
-      "assets/images/p1_3.jpg",
-      "assets/images/p1_4.jpg",
-    ],
-    category: "Clock , Wearable",
-    colors: [
-      {"colorName": "Red Rose", "color": const Color(0xFFF6625E)},
-      {"colorName": "Purple", "color": const Color(0xFF836DB8)},
-      {"colorName": "Gold", "color": const Color(0xFFDECB9C)},
-    ],
-    title: "Apple Watch",
-    price: 240.00,
-    description: {
-      "headline": "Get Apple TV+ free for a year",
-      "description": description
-    },
-    rating: 4.8,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 7,
-    image: "assets/images/p2.png",
-    images: [
-      "assets/images/p2_1.png",
-      "assets/images/p2_2.jpg",
-    ],
-    category: "Fashion",
-    colors: [
-      {"colorName": "Red Rose", "color": const Color(0xFFF6625E)},
-      {"colorName": "Purple", "color": const Color(0xFF836DB8)},
-      {"colorName": "Gold", "color": const Color(0xFFDECB9C)},
-      {"colorName": "Red", "color": const Color(0xE7FF0D29)}
-    ],
-    title: "Apple Watch",
-    price: 50.5,
-    description: {
-      "headline": "Get Apple TV+ free for a year",
-      "description": description
-    },
-    rating: 4.1,
-    isPopular: true,
-  ),
-  Product(
-    id: 3,
-    image: "assets/images/p3.png",
-    images: [
-      "assets/images/p3_1.png",
-    ],
-    category: "Shoes",
-    colors: [
-      {"colorName": "Red Rose", "color": const Color(0xFFF6625E)},
-      {"colorName": "Purple", "color": const Color(0xFF836DB8)},
-      {"colorName": "Gold", "color": const Color(0xFFDECB9C)},
-      {"colorName": "Red", "color": const Color(0xE7FF0D29)}
-    ],
-    title: "Nike Shoes",
-    price: 20.20,
-    description: {
-      "headline": "Get Nike shoes with another one as a gift",
-      "description": description
-    },
-    rating: 4.1,
-    isFavourite: false,
-  ),
-  Product(
-    id: 4,
-    image: "assets/images/p4.png",
-    images: [
-      "assets/images/p4.png",
-      "assets/images/p4.png",
-      "assets/images/p4.png",
-    ],
-    category: "Clocks",
-    colors: [
-      {"colorName": "Red Rose", "color": const Color(0xFFF6625E)},
-      {"colorName": "Purple", "color": const Color(0xFF836DB8)},
-      {"colorName": "Gold", "color": const Color(0xFFDECB9C)},
-      {"colorName": "Red", "color": const Color(0xE7FF0D29)}
-    ],
-    title: "Apple Watch",
-    price: 20.20,
-    description: {
-      "headline": "Get Apple TV+ free for a year",
-      "description": description
-    },
-    rating: 4.1,
-    isFavourite: false,
-  ),
-  Product(
-    id: 5,
-    image: "assets/images/p5_1.png",
-    images: [
-      "assets/images/p5_1.png",
-      "assets/images/p5_1.png",
-    ],
-    category: "Computers",
-    colors: [
-      {"colorName": "Red Rose", "color": const Color(0xFFF6625E)},
-      {"colorName": "Purple", "color": const Color(0xFF836DB8)},
-      {"colorName": "Gold", "color": const Color(0xFFDECB9C)},
-      {"colorName": "Red", "color": const Color(0xE7FF0D29)},
-      {"colorName": "Yellow", "color": const Color(0xE7FFD70D)}
-    ],
-    title: "Apple Laptop",
-    price: 36.55,
-    description: {
-      "headline": "Get Apple TV+ free for a year",
-      "description": description
-    },
-    rating: 4.4,
-    isFavourite: true,
-    isPopular: true,
-  ),
+  // Product(
+  //   id: 6,
+  //   image: "assets/images/p1.png",
+  //   images: [
+  //     "assets/images/p1_2.jpg",
+  //     "assets/images/p1_3.jpg",
+  //     "assets/images/p1_4.jpg",
+  //   ],
+  //   category: "Clock , Wearable",
+  //   colors: [
+  //     {"colorName": "Red Rose", "color": const Color(0xFFF6625E)},
+  //     {"colorName": "Purple", "color": const Color(0xFF836DB8)},
+  //     {"colorName": "Gold", "color": const Color(0xFFDECB9C)},
+  //   ],
+  //   title: "Apple Watch",
+  //   price: 240.00,
+  //   description: {
+  //     "headline": "Get Apple TV+ free for a year",
+  //     "description": description
+  //   },
+  //   rating: 4.8,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 7,
+  //   image: "assets/images/p2.png",
+  //   images: [
+  //     "assets/images/p2_1.png",
+  //     "assets/images/p2_2.jpg",
+  //   ],
+  //   category: "Fashion",
+  //   colors: [
+  //     {"colorName": "Red Rose", "color": const Color(0xFFF6625E)},
+  //     {"colorName": "Purple", "color": const Color(0xFF836DB8)},
+  //     {"colorName": "Gold", "color": const Color(0xFFDECB9C)},
+  //     {"colorName": "Red", "color": const Color(0xE7FF0D29)}
+  //   ],
+  //   title: "Apple Watch",
+  //   price: 50.5,
+  //   description: {
+  //     "headline": "Get Apple TV+ free for a year",
+  //     "description": description
+  //   },
+  //   rating: 4.1,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 3,
+  //   image: "assets/images/p3.png",
+  //   images: [
+  //     "assets/images/p3_1.png",
+  //   ],
+  //   category: "Shoes",
+  //   colors: [
+  //     {"colorName": "Red Rose", "color": const Color(0xFFF6625E)},
+  //     {"colorName": "Purple", "color": const Color(0xFF836DB8)},
+  //     {"colorName": "Gold", "color": const Color(0xFFDECB9C)},
+  //     {"colorName": "Red", "color": const Color(0xE7FF0D29)}
+  //   ],
+  //   title: "Nike Shoes",
+  //   price: 20.20,
+  //   description: {
+  //     "headline": "Get Nike shoes with another one as a gift",
+  //     "description": description
+  //   },
+  //   rating: 4.1,
+  //   isFavourite: false,
+  // ),
+  // Product(
+  //   id: 4,
+  //   image: "assets/images/p4.png",
+  //   images: [
+  //     "assets/images/p4.png",
+  //     "assets/images/p4.png",
+  //     "assets/images/p4.png",
+  //   ],
+  //   category: "Clocks",
+  //   colors: [
+  //     {"colorName": "Red Rose", "color": const Color(0xFFF6625E)},
+  //     {"colorName": "Purple", "color": const Color(0xFF836DB8)},
+  //     {"colorName": "Gold", "color": const Color(0xFFDECB9C)},
+  //     {"colorName": "Red", "color": const Color(0xE7FF0D29)}
+  //   ],
+  //   title: "Apple Watch",
+  //   price: 20.20,
+  //   description: {
+  //     "headline": "Get Apple TV+ free for a year",
+  //     "description": description
+  //   },
+  //   rating: 4.1,
+  //   isFavourite: false,
+  // ),
+  // Product(
+  //   id: 5,
+  //   image: "assets/images/p5_1.png",
+  //   images: [
+  //     "assets/images/p5_1.png",
+  //     "assets/images/p5_1.png",
+  //   ],
+  //   category: "Computers",
+  //   colors: [
+  //     {"colorName": "Red Rose", "color": const Color(0xFFF6625E)},
+  //     {"colorName": "Purple", "color": const Color(0xFF836DB8)},
+  //     {"colorName": "Gold", "color": const Color(0xFFDECB9C)},
+  //     {"colorName": "Red", "color": const Color(0xE7FF0D29)},
+  //     {"colorName": "Yellow", "color": const Color(0xE7FFD70D)}
+  //   ],
+  //   title: "Apple Laptop",
+  //   price: 36.55,
+  //   description: {
+  //     "headline": "Get Apple TV+ free for a year",
+  //     "description": description
+  //   },
+  //   rating: 4.4,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
 
 ];
 
